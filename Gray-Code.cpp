8d99dfbfbe80cca813be5af381c041a5a2107e01
San1estus <j.rodriguezisiordia@gmail.com>
2025-09-28 08:40:22 -0600

    Solucion Gray Code

--- Gray-Code.cpp
+++ Gray-Code.cpp
@@ -4,13 +4,32 @@
 #define MOD 1000000007
 using namespace std;
 
+vector<string> grayCode(int n){
+    if (n == 1){
+        return {"0", "1"};
+    }
+    vector<string> previous = grayCode(n-1);
+    vector<string> reversedPrevious = previous;
+    reverse(all(reversedPrevious));
+    int prevSz = sz(previous);
+    int index = 0;
+    
+    while(index < prevSz){
+        string zero = "0" + previous[index];
+        previous[index] = "1" + reversedPrevious[index];
+        previous.push_back(zero);
+        index++;
+    }
+    return previous;
+}
+
 int main(void){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int n;
     cin >> n;
-    vector<string> ans;
+    vector<string> ans = grayCode(n);
     
     for(string s: ans){
         cout << s << '\n';
